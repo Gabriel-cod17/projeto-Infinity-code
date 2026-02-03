00a5e4f802fee268ad8c9719f50341ddd684df91
Gabriel Felipe <gabrielssantiago1007@gmail.com>
2026-02-03 14:41:25 -0300

    correção menu mobile 6

--- assets/javascript/menuMobileAberto.js
+++ assets/javascript/menuMobileAberto.js
@@ -6,12 +6,12 @@ export function MenuAberto() {
     if (menu_Aberto.src == "")
       if (header_Mobile.classList.contains("header_Mobile_Entrou")) {
         header_Mobile.classList.remove("header_Mobile_Entrou");
-        menu_Aberto.src = "/assets/img/menu-aberto.png";
+        menu_Aberto.src = "assets/img/menu-aberto.png";
         menu_Aberto.classList.add("menu_aberto");
         menu_Aberto.classList.remove("menu_Fechado");
       } else {
         header_Mobile.classList.add("header_Mobile_Entrou");
-        menu_Aberto.src = "/assets/img/x.png";
+        menu_Aberto.src = "assets/img/x.png";
         menu_Aberto.classList.add("menu_Fechado");
         menu_Aberto.classList.remove("menu_aberto");
       }
