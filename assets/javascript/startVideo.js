1a92a890cda97b5df4cac425699515255ecba6ca
Gabriel Felipe <gabrielssantiago1007@gmail.com>
2026-02-03 11:10:02 -0300

    Alterações no video

--- assets/javascript/startVideo.js
+++ assets/javascript/startVideo.js
@@ -1,7 +1,8 @@
 export function startVideo() {
   const video = document.getElementById("video");
 
-  video.muted = true;
-  video.onplay().catch(() => {});
-  document.removeEventListener("touchstart", startVideo);
+  window.addEventListener("load", () => {
+    video.muted = true;
+    video.play().catch(() => {});
+  });
 }
