f9a3cad64213c85e7282fbd5b074b4039ab0bada
Gabriel Felipe <gabrielssantiago1007@gmail.com>
2026-02-03 14:39:03 -0300

    correção menu mobile 5

--- assets/javascript/menuMobileAberto.js
+++ assets/javascript/menuMobileAberto.js
@@ -3,16 +3,17 @@ export function MenuAberto() {
   const header_Mobile = document.getElementById("header_Mobile");
 
   menu_Aberto.addEventListener("click", () => {
-    if (header_Mobile.classList.contains("header_Mobile_Entrou")) {
-      header_Mobile.classList.remove("header_Mobile_Entrou");
-      menu_Aberto.src = "/assets/img/menu-aberto.png";
-      menu_Aberto.classList.add("menu_aberto");
-      menu_Aberto.classList.remove("menu_Fechado");
-    } else {
-      header_Mobile.classList.add("header_Mobile_Entrou");
-      menu_Aberto.src = "/assets/img/x.png";
-      menu_Aberto.classList.add("menu_Fechado");
-      menu_Aberto.classList.remove("menu_aberto");
-    }
+    if (menu_Aberto.src == "")
+      if (header_Mobile.classList.contains("header_Mobile_Entrou")) {
+        header_Mobile.classList.remove("header_Mobile_Entrou");
+        menu_Aberto.src = "/assets/img/menu-aberto.png";
+        menu_Aberto.classList.add("menu_aberto");
+        menu_Aberto.classList.remove("menu_Fechado");
+      } else {
+        header_Mobile.classList.add("header_Mobile_Entrou");
+        menu_Aberto.src = "/assets/img/x.png";
+        menu_Aberto.classList.add("menu_Fechado");
+        menu_Aberto.classList.remove("menu_aberto");
+      }
   });
 }
