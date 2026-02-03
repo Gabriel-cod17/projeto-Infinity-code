87eeb0d30064fce1afeb98cbda7597d8a6d2c425
Gabriel Felipe <gabrielssantiago1007@gmail.com>
2026-02-03 12:13:39 -0300

    Alterações no video parte 10

--- assets/javascript/startVideo.js
+++ assets/javascript/startVideo.js
@@ -1,11 +1,5 @@
 export function startVideo() {
   const video = document.getElementById("video");
-
-  window.addEventListener("load", () => {
-    video.muted = true;
-    video.play().catch(() => {});
-  });
-
   document.body.addEventListener("touchstart", () => {
     video.muted = true;
     video.play();
